c5daafbf368e0d34d8c6a6a70d949fb1416d4664
David Paiva <pillgp@pm.me>
2022-01-27 17:28:48 +0000

    Some changes idk

--- src/api.js
+++ src/api.js
@@ -101,13 +101,9 @@ module.exports = class Api {
 
     const sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect: "postgres",
-      ssl: true,
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
+      ssl: {
+        rejectUnauthorized: false,
+      }
     });
 
     try {
