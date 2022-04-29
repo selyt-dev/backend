950409282dca1e94a80c05de4619deb6df210808
David Paiva <davidsgpaiva@gmail.com>
2022-04-29 14:53:33 +0100

    🚨  Getting the linter to not yell at me. V2.

--- src/api.js
+++ src/api.js
@@ -57,7 +57,7 @@ module.exports = class Api {
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
-      }
+      },
     });
 
     this.mailer.verify((error, success) => {
