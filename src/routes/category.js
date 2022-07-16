f8b348571ce780156a79e4120b9f66043dd2e527
David Paiva <pillgp@pm.me>
2022-07-16 23:57:50 +0000

    ✨ Added admin route to include non-active categories.

--- src/routes/category.js
+++ src/routes/category.js
@@ -46,6 +46,35 @@ module.exports = class Category extends Route {
       }
     );
 
+    router.get(
+      "/include-non-active",
+      this.client.routeUtils.validateLogin(this.client),
+      async (req, res) => {
+        let { page, limit, orderBy } = req.query;
+
+        page = parseInt(page) || 1;
+        limit = parseInt(limit) || 10;
+
+        orderBy = orderBy ? orderBy.split(" ") : ["name", "ASC"];
+        orderBy[0] = this.lowerCaseFirstLetter(orderBy[0]);
+        orderBy[1] = orderBy[1] || "ASC";
+
+        const offset = limit * (page - 1);
+
+        const categories = await this.client.database.models.Category.findAll({
+          order: [orderBy],
+          offset,
+          limit,
+        });
+
+        const count = await this.client.database.models.Category.count();
+
+        return res
+          .status(200)
+          .json({ ok: true, categories, page, limit, count });
+      }
+    );
+
     router.post(
       "/",
       this.client.routeUtils.validateLoginAdmin(this.client),
